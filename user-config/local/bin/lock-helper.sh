00af22ceadf2435f7cb1ecfde4e1062a31fd9e25
Kristofer Hansson <koffkoffh@gmail.com>
2026-01-08 05:02:34 +0100

    added missing script.

--- user-config/local/bin/lock-helper.sh
+++ user-config/local/bin/lock-helper.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-# Mutes on before locking, then locks using the cmd passed as arguments, upon unlock,
-# unmutes if audio was unmuted before locking. The locking and unmuting forks to
-# background in order for any idle process to be able to control screens (dpms).
+# Makes sure the system is muted on "lock", restores previous state un "unlock".
 
 state_file="${XDG_RUNTIME_DIR:-/tmp}/was_mute"
 
 mute() {
-	~/.local/bin/spotify-ctl pause
+	playerctl -a pause
 	# Check if currently unmuted
 	if wpctl get-volume "@DEFAULT_AUDIO_SINK@" | grep -q '\[MUTED\]'; then
 		: > "$state_file"
@@ -24,8 +22,7 @@ unmute() {
 	fi
 }
 
-mute
-{
-	"$@"
-	unmute
-} &
+case "$1" in
+	lock) mute ;;
+	unlock) unmute ;;
+esac
